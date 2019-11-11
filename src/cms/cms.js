58292032df8921031b6806d61d169faf69fea513
Pham Bien <pham.itdlu@gmail.com>
2019-11-11 16:48:13 +0700

    update

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CMS from 'netlify-cms'
 import './cms-utils'
+import uuid from 'uuid/v4';
 
 import { HomePageTemplate } from '../templates/HomePage'
 import { ComponentsPageTemplate } from '../templates/ComponentsPage'
@@ -38,3 +39,35 @@ CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
 CMS.registerPreviewTemplate('posts', ({ entry }) => (
   <SinglePostTemplate {...entry.toJS().data} />
 ))
+
+/**
+ * Create the control widget, this will add a form element to the cms UI
+ */
+const IdControl = window.createClass({  
+    getInitialState: function() {    return {};  },
+    componentDidMount: function() {
+        // If this widget doesn't have an ID yet we create one using the UUID package    
+        if (!this.props.value) { 
+            this.props.onChange(uuid());    
+        } 
+    }, 
+    handleChange() { 
+        this.props.onChange(uuid());
+    },  
+    render: function() { 
+        return window.h('p', null, `${this.props.value}`);
+    }
+});
+
+/**
+ * Create the preview widget, this will display the widgets value in the NetlifyCMS preview pane
+ */
+const IdPreview = window.createClass({  
+    getInitialState: function() { console.log(this.props); return {}; }, 
+    render: function() { 
+        return window.h('p', null, `ID: ${this.props.value}`);  
+    }
+});
+
+// Register the widget. This lets NetlifyCMS know about our custom widget
+CMS.registerWidget('id', IdControl, IdPreview);
